c8b9f7fe98353a3939279669bce11a6033bd05d0
Bui Nhat Phi <phithcsof798@gmail.com>
2026-03-08 11:41:36 +0700

    new logo & fix bug

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -19,12 +19,10 @@ extensions = []
 templates_path = ['_templates']
 exclude_patterns = []
 
-import sphinx_rtd_theme
-html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
-
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_rtd_theme'
+html_logo = "_static/logo.png"
 html_static_path = ['_static']
