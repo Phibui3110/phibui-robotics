b1ffdd55b767ef9a2b2beffb25cd590c18a38722
Bui Nhat Phi <phithcsof798@gmail.com>
2026-03-14 10:19:22 +0700

    camera pipe line

---
+++ ros2_ws/src/camera_pipeline/camera_pipeline/camera_subscriber.py
@@ -0,0 +1,51 @@
+import rclpy
+from rclpy.node import Node
+
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge
+
+import cv2
+
+
+class CameraSubscriber(Node):
+
+    def __init__(self):
+
+        super().__init__('camera_subscriber')
+
+        self.bridge = CvBridge()
+
+        self.subscription = self.create_subscription(
+            Image,
+            '/image_raw',
+            self.image_callback,
+            10
+        )
+
+        self.get_logger().info("Camera subscriber node started")
+
+
+    def image_callback(self, msg):
+
+        frame = self.bridge.imgmsg_to_cv2(msg, desired_encoding='bgr8')
+
+        cv2.imshow("Camera Stream", frame)
+
+        cv2.waitKey(1)
+
+
+def main(args=None):
+
+    rclpy.init(args=args)
+
+    node = CameraSubscriber()
+
+    rclpy.spin(node)
+
+    node.destroy_node()
+
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
